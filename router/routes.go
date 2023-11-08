77c85bb6278ec6f93366b6170de1c0ac15ece27d
Felipe Sousa Developer <fesousalocutor@gmail.com>
2023-11-08 00:39:34 -0300

    feat: add routes in verbs

--- router/routes.go
+++ router/routes.go
@@ -8,10 +8,30 @@ import (
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
-	{
+	{		
 		v1.GET("/opening", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"msg": "GO primary routes",
+			})
+		})
+		v1.POST("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "GO primary routes",
+			})
+		})
+		v1.DELETE("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "GO primary routes",
+			})
+		})
+		v1.PUT("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "GO primary routes",
+			})
+		})
+		v1.GET("/openings", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "All",
 			})
 		})
 	}
