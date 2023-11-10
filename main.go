2cd10772253bac46c1c65c735fbf53043e062ff0
Felipe Sousa Developer <fesousalocutor@gmail.com>
2023-11-10 10:54:34 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/felipefernandesdev/api_opportunities/router"
+import (
+	"github.com/felipefernandesdev/api_opportunities/config"
+	"github.com/felipefernandesdev/api_opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("Main")
+	//Initialize Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initializate error: %v", err)
+		return
+	}
+
 	//Initialize Router
 	router.Initialize()
 }
